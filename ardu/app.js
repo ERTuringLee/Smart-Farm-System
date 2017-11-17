0516aa75a365c479fcdac4cb3c1770f2275b9d3a
Lee JaeMoon <31405723+ErwinRomelLee@users.noreply.github.com>
2017-11-17 11:04:32 +0900

    Update app.js

--- ardu/app.js
+++ ardu/app.js
@@ -23,13 +23,13 @@ var pool = mysql.createPool({
   database : ''
 });
 var port = new SerialPort('/dev/ttyACM1', {
-	baudRate: 9600
+  baudRate: 9600
 });
 var port1 = new SerialPort('/dev/rfcomm1', {
-	baudRate: 9600
+  baudRate: 9600
 });
 var port2 = new SerialPort('/dev/rfcomm2', {
-	baudRate: 9600
+  baudRate: 9600
 });
 var line = ''
 var line1 = ''
@@ -61,43 +61,43 @@ var sensor_value = '';
 var sensor_value1 = '';
 var sensor_value2 = '';
 port.on('data', function(data){
-	var decoder = new StringDecoder('utf8');
-	var textData = decoder.write(data);
-	line += textData
-	var li = line.split("\n");
-	if(li.length>1){
-		sensor_value = li[li.length-2]
-		line = ''
-	}
+  var decoder = new StringDecoder('utf8');
+  var textData = decoder.write(data);
+  line += textData
+  var li = line.split("\n");
+  if(li.length>1){
+    sensor_value = li[li.length-2]
+    line = ''
+  }
 })
 port1.on('data', function(data){
-	var decoder1 = new StringDecoder('utf8');
-	var textData1 = decoder1.write(data)
-	line1 += textData1
-	var li1 = line1.split('\n')
-	if(li1.length>1){
-		sensor_value1 = li1[0]
-		line1 = ''
-	}
+  var decoder1 = new StringDecoder('utf8');
+  var textData1 = decoder1.write(data)
+  line1 += textData1
+  var li1 = line1.split('\n')
+  if(li1.length>1){
+    sensor_value1 = li1[0]
+    line1 = ''
+  }
 })
 port2.on('data', function(data){
-	var decoder2 = new StringDecoder('utf8');
-	var textData2 = decoder2.write(data);
-	line2 += textData2
-	var li2 = line2.split('\n');
-	if(li2.length>1){
-		sensor_value2 = li2[0]
-		line2 = ''
-	}
+  var decoder2 = new StringDecoder('utf8');
+  var textData2 = decoder2.write(data);
+  line2 += textData2
+  var li2 = line2.split('\n');
+  if(li2.length>1){
+    sensor_value2 = li2[0]
+    line2 = ''
+  }
 });
 app.get('/sensor', function(request, response){
-	response.send(sensor_value);
+  response.send(sensor_value);
 });
 app.get('/sensor1', function(request, response){
-	response.send(sensor_value1);
+  response.send(sensor_value1);
 });
 app.get('/sensor2', function(request, response){
-	response.send(sensor_value2);
+  response.send(sensor_value2);
 });
 app.get('/led', function(req, res, next){
   if(sess==null){
@@ -153,127 +153,112 @@ app.get('/logout',function(request,response){
 app.get('/led/:switch', function(req, res, next){
   console.log(sess);
   if(sess==null){
-	res.redirect('/login');	
+    res.redirect('/login');	
   }else{
-//	if(sess.name==undefined){
-//		res.redirect('/login');
-//  	}else{
-    		function setLED(flag) {
-      			var fs = require('fs');
-      			fs.open('/dev/ttyACM0','a', 666, function(e, fd) {
-        		fs.write(fd, flag ? '1' : '0', null, null, null, function() {
-         			fs.close(fd, function() { });
-        		});
-      		});
-//	}
+    function setLED(flag) {
+      var fs = require('fs');
+      fs.open('/dev/ttyACM0','a', 666, function(e, fd) {
+        fs.write(fd, flag ? '1' : '0', null, null, null, function() {
+          fs.close(fd, function() { });
+        });
+      });
     }
     var onoff = req.params.switch;
     if(onoff == 'on') setLED(0);
     if(onoff == 'off') setLED(1);
     res.render('led', { title: 'Solenoid Control : ' + req.params.switch });
   }
-  
 });
 app.post('/solarstatus', function(request, response){
-	function setSolar(flag){
-		var fs = require('fs');
-		fs.open('/dev/rfcomm0', 'a', 666, function(e, fd){
-			if(flag==0){
-				fs.write(fd, '0', null, null, null, function(){
-					fs.close(fd, function(){});
-				});
-			}
-			if(flag==1){
-				fs.write(fd, '1', null, null, null, function(){
-					fs.close(fd, function(){});
-				});
-			}
-			if(flag==2){
-				fs.write(fd, '2', null, null, null, function(){
-					fs.close(fd, function(){});
-				});
-			}
-		});
-	}
-	var switchs = request.body.switchs;
-	if(switchs=='off'){
-		setSolar(0);
-		response.redirect('/');
-	}
-	if(switchs=='on'){
-		setSolar(1);
-		response.redirect('/');
-	}
-	if(switchs=='auto'){
-		setSolar(2);
-		response.redirect('/');
-	}
+  function setSolar(flag){
+    var fs = require('fs');
+    fs.open('/dev/rfcomm0', 'a', 666, function(e, fd){
+      if(flag==0){
+        fs.write(fd, '0', null, null, null, function(){
+          fs.close(fd, function(){});
+        });
+      }
+      if(flag==1){
+        fs.write(fd, '1', null, null, null, function(){
+          fs.close(fd, function(){});
+        });
+      }
+      if(flag==2){
+        fs.write(fd, '2', null, null, null, function(){
+          fs.close(fd, function(){});
+        });
+      }
+    });
+  }
+  var switchs = request.body.switchs;
+  if(switchs=='off'){
+    setSolar(0);
+    response.redirect('/');
+  }
+  if(switchs=='on'){
+    setSolar(1);
+    response.redirect('/');
+  }
+  if(switchs=='auto'){
+    setSolar(2);
+    response.redirect('/');
+  }
 });
 app.post('/onoff', function(request, response) {
-	function setLED(flag){
-		var fs = require('fs');
-		fs.open('/dev/ttyACM0', 'a', 666, function(e, fd) {
-			fs.write(fd, flag ? '1' : '0', null, null, null, function () {
-				fs.close(fd, function() { });
-			});
-		});
-	}
-	var d = new Date()
-	var time = d.getFullYear() + '.'+(d.getMonth()+1)+'.'+d.getDate()+'.'+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
-	var switchs = request.body.switchs;
-	var st_time;
-	if(switchs=='on'){
-		pool.getConnection(function(err, connection){
-			connection.query('insert into on_off_time (time, status) values("'+time+'", "'+switchs+'");', function(err, data){
-				connection.release();
-			})
-		})
-	 	setLED(0);
-		st_time = setTimeout(function(){
-			setLED(1);
-			pool.getConnection(function(err, connection){
-				var stop_time = new Date()
-				var time2 = stop_time.getFullYear()+'.'+(stop_time.getMonth()+1)+'.'+stop_time.getDate()+'.'+stop_time.getHours()+':'+stop_time.getMinutes()+':'+stop_time.getSeconds();
-				connection.query('insert into on_off_time (time, status) values("'+time2+'", "off");', function(err, data){
-				connection.release();
-				})
-			})
-		}, 1800000)	
-		response.redirect('/led');
-			
-
-	}
-	if(switchs=='off'){
-		pool.getConnection(function(err, connection){
-			connection.query('insert into on_off_time (time, status) values("'+time+'", "'+switchs+'");', function(err, data){
-				connection.release();
-			})
-		})
-	 	setLED(1);
-		response.redirect('/led')
-	}
-	
+  function setLED(flag){
+    var fs = require('fs');
+    fs.open('/dev/ttyACM0', 'a', 666, function(e, fd) {
+      fs.write(fd, flag ? '1' : '0', null, null, null, function () {
+        fs.close(fd, function() { });
+      });
+    });
+  }
+  var d = new Date()
+  var time = d.getFullYear() + '.'+(d.getMonth()+1)+'.'+d.getDate()+'.'+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
+  var switchs = request.body.switchs;
+  var st_time;
+  if(switchs=='on'){
+    pool.getConnection(function(err, connection){
+      connection.query('insert into on_off_time (time, status) values("'+time+'", "'+switchs+'");', function(err, data){
+        connection.release();
+      })
+    })
+    setLED(0);
+    st_time = setTimeout(function(){
+      setLED(1);
+      pool.getConnection(function(err, connection){
+        var stop_time = new Date()
+        var time2 = stop_time.getFullYear()+'.'+(stop_time.getMonth()+1)+'.'+stop_time.getDate()+'.'+stop_time.getHours()+':'+stop_time.getMinutes()+':'+stop_time.getSeconds();
+        connection.query('insert into on_off_time (time, status) values("'+time2+'", "off");', function(err, data){
+          connection.release();
+        })
+      })
+    }, 1800000)	
+    response.redirect('/led');
+  }
+  if(switchs=='off'){
+    pool.getConnection(function(err, connection){
+      connection.query('insert into on_off_time (time, status) values("'+time+'", "'+switchs+'");', function(err, data){
+        connection.release();
+      })
+    })
+    setLED(1);
+    response.redirect('/led')
+  }
 });
 app.get('/record', function(request, response){
-	pool.getConnection(function(err, connection){
-		connection.query('SELECT * from on_off_time order by id desc', function(err, data){
-			response.send(data);
-			connection.release();
-		})
-	})
+  pool.getConnection(function(err, connection){
+    connection.query('SELECT * from on_off_time order by id desc', function(err, data){
+      response.send(data);
+      connection.release();
+    })
+  })
 })
-//////login end
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -293,7 +278,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-
 
 module.exports = app;
